6c7d081508574a23668fe0c27cc738c228b4668e
JexUK <cmrxn2005@gmail.com>
2021-12-23 17:53:03 +0000

    Move particles to LoginFormContainer

--- resources/scripts/components/auth/LoginFormContainer.tsx
+++ resources/scripts/components/auth/LoginFormContainer.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components/macro';
 import { breakpoint } from '@/theme';
 import FlashMessageRender from '@/components/FlashMessageRender';
 import tw from 'twin.macro';
+import Particles from 'react-tsparticles';
 
 const Wrapper = styled.div`
     ${breakpoint('sm')`
@@ -33,18 +34,131 @@ const Inner = ({ children }: { children: React.ReactNode }) => (
 );
 
 const Container = ({ title, children }: { title?: string, children: React.ReactNode }) => (
-    <Wrapper>
-        {title &&
-        <h2 css={tw`text-3xl text-center text-neutral-100 font-medium py-4`}>
-            {title}
-        </h2>
-        }
-        <FlashMessageRender css={tw`mb-2 px-1`}/>
-        {children}
-        <p css={tw`text-center text-neutral-500 text-xs mt-4`}>
-            &copy; {(new Date()).getFullYear()}&nbsp;Jexactyl
-        </p>
-    </Wrapper>
+    <div>
+        <Particles
+            params={{
+                particles: {
+                    number: {
+                        value: 40,
+                        density: {
+                            enable: true,
+                            value_area: 800,
+                        },
+                    },
+                    color: {
+                        value: '#ffffff',
+                    },
+                    shape: {
+                        type: 'circle',
+                        stroke: {
+                            width: 0,
+                            color: '#000000',
+                        },
+                        polygon: {
+                            nb_sides: 5,
+                        },
+                        image: {
+                            src: 'img/github.svg',
+                            width: 100,
+                            height: 100,
+                        },
+                    },
+                    opacity: {
+                        value: 0.40246529723245905,
+                        random: false,
+                        anim: {
+                            enable: false,
+                            speed: 1,
+                            opacity_min: 0.1,
+                            sync: false,
+                        },
+                    },
+                    size: {
+                        value: 1,
+                        random: true,
+                        anim: {
+                            enable: false,
+                            speed: 40,
+                            size_min: 0.1,
+                            sync: false,
+                        },
+                    },
+                    line_linked: {
+                        enable: true,
+                        distance: 150,
+                        color: '#ffffff',
+                        opacity: 0.4,
+                        width: 1,
+                    },
+                    move: {
+                        enable: true,
+                        speed: 3,
+                        direction: 'none',
+                        random: false,
+                        straight: false,
+                        out_mode: 'out',
+                        bounce: false,
+                        attract: {
+                            enable: false,
+                            rotateX: 600,
+                            rotateY: 1200,
+                        },
+                    },
+                },
+                interactivity: {
+                    detect_on: 'window',
+                    events: {
+                        onhover: {
+                            enable: true,
+                            mode: 'repulse',
+                        },
+                        onclick: {
+                            enable: false,
+                            mode: 'repulse',
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        grab: {
+                            distance: 400,
+                            line_linked: {
+                                opacity: 1,
+                            },
+                        },
+                        bubble: {
+                            distance: 400,
+                            size: 40,
+                            duration: 2,
+                            opacity: 8,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                        push: {
+                            particles_nb: 4,
+                        },
+                        remove: {
+                            particles_nb: 2,
+                        },
+                    },
+                },
+                retina_detect: true,
+            }}
+        />
+        <Wrapper>
+            {title &&
+            <h2 css={tw`text-3xl text-center text-neutral-100 font-medium py-4`}>
+                {title}
+            </h2>
+            }
+            <FlashMessageRender css={tw`mb-2 px-1`}/>
+            {children}
+            <p css={tw`text-center text-neutral-500 text-xs mt-4`}>
+                &copy; {(new Date()).getFullYear()}&nbsp;Jexactyl
+            </p>
+        </Wrapper>
+    </div>
 );
 
 type FormContainerProps = React.DetailedHTMLProps<React.FormHTMLAttributes<HTMLFormElement>, HTMLFormElement> & {
--- resources/scripts/routers/AuthenticationRouter.tsx
+++ resources/scripts/routers/AuthenticationRouter.tsx
@@ -9,142 +9,28 @@ import ResetPasswordContainer from '@/components/auth/ResetPasswordContainer';
 import RegisterContainer from '@/components/auth/RegisterContainer';
 import { NotFound } from '@/components/elements/ScreenBlock';
 import { useStoreState } from 'easy-peasy';
-import Particles from 'react-tsparticles';
 
 export default ({ location, history, match }: RouteComponentProps) => {
     const userRegistration = useStoreState(state => state.settings.data!.userRegistration);
 
     return (
-        <div>
-            <Particles
-                params={{
-                    particles: {
-                        number: {
-                            value: 40,
-                            density: {
-                                enable: true,
-                                value_area: 800,
-                            },
-                        },
-                        color: {
-                            value: '#ffffff',
-                        },
-                        shape: {
-                            type: 'circle',
-                            stroke: {
-                                width: 0,
-                                color: '#000000',
-                            },
-                            polygon: {
-                                nb_sides: 5,
-                            },
-                            image: {
-                                src: 'img/github.svg',
-                                width: 100,
-                                height: 100,
-                            },
-                        },
-                        opacity: {
-                            value: 0.40246529723245905,
-                            random: false,
-                            anim: {
-                                enable: false,
-                                speed: 1,
-                                opacity_min: 0.1,
-                                sync: false,
-                            },
-                        },
-                        size: {
-                            value: 1,
-                            random: true,
-                            anim: {
-                                enable: false,
-                                speed: 40,
-                                size_min: 0.1,
-                                sync: false,
-                            },
-                        },
-                        line_linked: {
-                            enable: true,
-                            distance: 150,
-                            color: '#ffffff',
-                            opacity: 0.4,
-                            width: 1,
-                        },
-                        move: {
-                            enable: true,
-                            speed: 3,
-                            direction: 'none',
-                            random: false,
-                            straight: false,
-                            out_mode: 'out',
-                            bounce: false,
-                            attract: {
-                                enable: false,
-                                rotateX: 600,
-                                rotateY: 1200,
-                            },
-                        },
-                    },
-                    interactivity: {
-                        detect_on: 'window',
-                        events: {
-                            onhover: {
-                                enable: true,
-                                mode: 'repulse',
-                            },
-                            onclick: {
-                                enable: false,
-                                mode: 'repulse',
-                            },
-                            resize: true,
-                        },
-                        modes: {
-                            grab: {
-                                distance: 400,
-                                line_linked: {
-                                    opacity: 1,
-                                },
-                            },
-                            bubble: {
-                                distance: 400,
-                                size: 40,
-                                duration: 2,
-                                opacity: 8,
-                            },
-                            repulse: {
-                                distance: 200,
-                                duration: 0.4,
-                            },
-                            push: {
-                                particles_nb: 4,
-                            },
-                            remove: {
-                                particles_nb: 2,
-                            },
-                        },
-                    },
-                    retina_detect: true,
-                }}
-            />
-            <div className={'pt-8 xl:pt-32'}>
-                <TransitionRouter>
-                    <Switch location={location}>
-                        {userRegistration === '1' &&
+        <div className={'pt-8 xl:pt-32'}>
+            <TransitionRouter>
+                <Switch location={location}>
+                    {userRegistration === '1' &&
                             <Route path={`${match.path}/register`} component={RegisterContainer} exact/>
-                        }
-                        <Route path={`${match.path}/login`} component={LoginContainer} exact/>
-                        <Route path={`${match.path}/login/checkpoint`} component={LoginCheckpointContainer}/>
-                        <Route path={`${match.path}/login/key`} component={LoginKeyCheckpointContainer}/>
-                        <Route path={`${match.path}/password`} component={ForgotPasswordContainer} exact/>
-                        <Route path={`${match.path}/password/reset/:token`} component={ResetPasswordContainer}/>
-                        <Route path={`${match.path}/checkpoint`}/>
-                        <Route path={'*'}>
-                            <NotFound onBack={() => history.push('/auth/login')}/>
-                        </Route>
-                    </Switch>
-                </TransitionRouter>
-            </div>
+                    }
+                    <Route path={`${match.path}/login`} component={LoginContainer} exact/>
+                    <Route path={`${match.path}/login/checkpoint`} component={LoginCheckpointContainer}/>
+                    <Route path={`${match.path}/login/key`} component={LoginKeyCheckpointContainer}/>
+                    <Route path={`${match.path}/password`} component={ForgotPasswordContainer} exact/>
+                    <Route path={`${match.path}/password/reset/:token`} component={ResetPasswordContainer}/>
+                    <Route path={`${match.path}/checkpoint`}/>
+                    <Route path={'*'}>
+                        <NotFound onBack={() => history.push('/auth/login')}/>
+                    </Route>
+                </Switch>
+            </TransitionRouter>
         </div>
     );
 };
