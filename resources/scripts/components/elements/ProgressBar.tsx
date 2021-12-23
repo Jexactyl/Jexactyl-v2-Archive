c61fd42ac670a46df3abbaf30e41226b5af596cb
Jex <cmrxn2005@gmail.com>
2021-12-23 21:28:25 +0000

    Test: Rewrite ProgressBar to use CSS better

--- resources/scripts/components/elements/ProgressBar.tsx
+++ resources/scripts/components/elements/ProgressBar.tsx
@@ -1,17 +1,20 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components/macro';
 import { useStoreActions, useStoreState } from 'easy-peasy';
 import { randomInt } from '@/helpers';
+import { CSSTransition } from 'react-transition-group';
 import tw from 'twin.macro';
 
 const BarFill = styled.div`
     ${tw`h-full`};
+    background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);
     transition: 250ms ease-in-out;
 `;
 
 export default () => {
     const interval = useRef<number>(null);
     const timeout = useRef<number>(null);
+    const [ visible, setVisible ] = useState(false);
     const progress = useStoreState(state => state.progress.progress);
     const continuous = useStoreState(state => state.progress.continuous);
     const setProgress = useStoreActions(actions => actions.progress.setProgress);
@@ -24,6 +27,8 @@ export default () => {
     }, []);
 
     useEffect(() => {
+        setVisible((progress || 0) > 0);
+
         if (progress === 100) {
             // @ts-ignore
             timeout.current = setTimeout(() => setProgress(undefined), 500);
@@ -54,42 +59,16 @@ export default () => {
     }, [ progress, continuous ]);
 
     return (
-        <div css={tw`w-full fixed`} style={{ height: '7px' }}>
-            <BarFill
-                key={'bar1'}
-                css={'bg-rainbow-50'}
-                style={{ height: '1px', width: progress === undefined ? '100%' : `${progress}%` }}
-            />
-            <BarFill
-                key={'bar2'}
-                css={'bg-rainbow-100'}
-                style={{ height: '2px', width: progress === undefined ? '100%' : `${progress}%` }}
-            />
-            <BarFill
-                key={'bar3'}
-                css={'bg-rainbow-150'}
-                style={{ height: '3px', width: progress === undefined ? '100%' : `${progress}%` }}
-            />
-            <BarFill
-                key={'bar4'}
-                css={'bg-rainbow-200'}
-                style={{ height: '4px', width: progress === undefined ? '100%' : `${progress}%` }}
-            />
-            <BarFill
-                key={'bar5'}
-                css={'bg-rainbow-250'}
-                style={{ height: '5px', width: progress === undefined ? '100%' : `${progress}%` }}
-            />
-            <BarFill
-                key={'bar6'}
-                css={'bg-rainbow-300'}
-                style={{ height: '6px', width: progress === undefined ? '100%' : `${progress}%` }}
-            />
-            <BarFill
-                key={'bar7'}
-                css={'bg-rainbow-350'}
-                style={{ height: '7px', width: progress === undefined ? '100%' : `${progress}%` }}
-            />
+        <div css={tw`w-full fixed`} style={{ height: '2px' }}>
+            <CSSTransition
+                timeout={150}
+                appear
+                in={visible}
+                unmountOnExit
+                classNames={'fade'}
+            >
+                <BarFill style={{ width: progress === undefined ? '100%' : `${progress}%` }}/>
+            </CSSTransition>
         </div>
     );
 };
