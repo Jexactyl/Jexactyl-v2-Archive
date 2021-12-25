9d17790ac05c4b21ac9e29b32c683208cd64edc0
Liam L <thefallenspirit@outlook.com>
2021-12-25 14:17:08 -0800

    Added CreditsFormRequest; Updated CreditsController.

---
+++ app/Http/Requests/Admin/CreditsFormRequest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pterodactyl\Http\Requests\Admin;
+
+class CreditsFormRequest extends AdminFormRequest
+{
+    public function rules()
+    {
+        return [
+            'enabled' => 'required|bool'
+        ]
+    }
+}
