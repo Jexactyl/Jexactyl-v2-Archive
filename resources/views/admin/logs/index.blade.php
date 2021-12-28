18adf69855761b187901e283393e49de1e2bf07a
JexUK <cmrxn2005@gmail.com>
2021-12-28 01:59:06 +0000

    Fix formatting of logs page part 2

--- resources/views/admin/logs/index.blade.php
+++ resources/views/admin/logs/index.blade.php
@@ -13,10 +13,11 @@
 @endsection
 
 @section('content')
-    <div class="row">
+<div class="row">
+    <div class="col-xs-12">
         <div class="box box-primary">
             <div class="box-header with-border">
-                <h3 class="box-title">Logs</h3>
+                <h3 class="box-title">Audit Logs</h3>
             </div>
             <div class="box-body table-responsive no-padding">
                  <table class="table table-hover">
@@ -42,4 +43,4 @@
             </div>
         </div>
     </div>
-@endsection
+</div>
