9911c58c1a565a43adfb840db28d2c897892a67d
JexUK <cmrxn2005@gmail.com>
2022-01-03 03:18:24 +0000

    Create API endpoint and display user credit amount in store

---
+++ resources/scripts/api/account/getCrBalance.ts
@@ -0,0 +1,14 @@
+import http from '@/api/http';
+
+export interface CrBalanceData {
+    // eslint-disable-next-line camelcase
+    cr_balance: string;
+}
+
+export default (): Promise<CrBalanceData> => {
+    return new Promise((resolve, reject) => {
+        http.get('/api/client/account/')
+            .then(({ data }) => resolve(data.data))
+            .catch(reject);
+    });
+};
--- resources/scripts/components/store/UserInformationRow.tsx
+++ resources/scripts/components/store/UserInformationRow.tsx
@@ -1,16 +1,33 @@
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import tw from 'twin.macro';
 import TitledGreyBox from '@/components/elements/TitledGreyBox';
+import getCrBalance, { CrBalanceData } from '@/api/account/getCrBalance';
+import { httpErrorToHuman } from '@/api/http';
+import { ApplicationStore } from '@/state';
+import { useStoreActions, Actions } from 'easy-peasy';
 
 const UserInformationRow = () => {
+    const [ data, setData ] = useState<CrBalanceData>();
+    const { addError, clearFlashes } = useStoreActions((actions: Actions<ApplicationStore>) => actions.flashes);
+
+    useEffect(() => {
+        clearFlashes('account');
+        getCrBalance()
+            .then(data => setData(data))
+            .catch(error => {
+                console.error(error);
+                addError({ key: 'account', message: httpErrorToHuman(error) });
+            });
+    }, []);
+
     return (
         <TitledGreyBox
             title={'User Information'}
             icon={faInfoCircle}
             css={tw`flex-1`}
         >
-            <h3 css={tw`text-2xl`}>Placeholder</h3>
+            You have <h3 css={tw`text-2xl`}>{data?.cr_balance}</h3> credits available.
         </TitledGreyBox>
     );
 };
