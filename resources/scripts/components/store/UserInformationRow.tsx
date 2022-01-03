b38e406e2b925b9347fbfd807ec560c0b6a3be86
JexUK <cmrxn2005@gmail.com>
2022-01-03 13:36:58 +0000

    Use State over API calls

--- resources/scripts/components/store/UserInformationRow.tsx
+++ resources/scripts/components/store/UserInformationRow.tsx
@@ -1,30 +1,11 @@
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import tw from 'twin.macro';
 import TitledGreyBox from '@/components/elements/TitledGreyBox';
-import getCrBalance, { CrBalanceData } from '@/api/account/getCrBalance';
-import { httpErrorToHuman } from '@/api/http';
-import { ApplicationStore } from '@/state';
-import { useStoreActions, Actions } from 'easy-peasy';
 import { useStoreState } from '@/state/hooks';
 
 const UserInformationRow = () => {
     const crBalance = useStoreState(state => state.user.data!.crBalance);
-    const [ data, setData ] = useState<CrBalanceData>();
-    const { addError, clearFlashes } = useStoreActions((actions: Actions<ApplicationStore>) => actions.flashes);
-
-    useEffect(() => {
-        clearFlashes('account');
-        getCrBalance()
-            .then(data => setData(data))
-            .catch(error => {
-                console.error(error);
-                addError({ key: 'account', message: httpErrorToHuman(error) });
-            });
-    }, []);
-
-    // Debug log to make sure API works!
-    console.log(data);
 
     return (
         <TitledGreyBox
@@ -32,7 +13,7 @@ const UserInformationRow = () => {
             icon={faInfoCircle}
             css={tw`flex-1`}
         >
-            You have <h3 css={tw`text-2xl`}>API: {data?.cr_balance} State: {crBalance}</h3> credits available.
+            <div>You have <h3 css={tw`text-2xl`}>{crBalance}</h3> credits available.</div>
         </TitledGreyBox>
     );
 };
