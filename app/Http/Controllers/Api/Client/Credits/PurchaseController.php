3c99fbb3dc0b63a00da3c5977526a947a7297dd4
TheFallenSpirit <thefallenspirit@outlook.com>
2022-01-09 14:14:52 -0800

    Fix PurchaseController

--- app/Http/Controllers/Api/Client/Credits/PurchaseController.php
+++ app/Http/Controllers/Api/Client/Credits/PurchaseController.php
@@ -2,37 +2,36 @@
 
 namespace Pterodactyl\Http\Controllers\Api\Client\Credits;
 
-use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Validation\ValidationException;
-use Pterodactyl\Exceptions\DisplayException;
-use Pterodactyl\Contracts\Repository\CreditsRepositoryInterface;
-use Pterodactyl\Exceptions\Repository\RecordNotFoundException;
-use Pterodactyl\Exceptions\Service\Deployment\NoViableAllocationException;
-use Pterodactyl\Exceptions\Service\Deployment\NoViableNodeException;
-use Pterodactyl\Http\Controllers\Api\Client\ClientApiController;
 use Pterodactyl\Http\Requests\Api\Client\StoreRequest;
-use Pterodactyl\Models\Node;
-use Pterodactyl\Services\Servers\ServerCreationService;
-use Throwable;
+use Pterodactyl\Contracts\Repository\CreditsRepositoryInterface;
+use Pterodactyl\Http\Controllers\Api\Client\ClientApiController;
 
 class PurchaseController extends ClientApiController
 {
     public CreditsRepositoryInterface $credits;
 
-    /**
-     * @throws DisplayException
-     */
+    public function __construct(CreditsRepositoryInterface $credits)
+    {
+        parent::__construct();
+        $this->credits = $credits;
+    }
+
     public function buyCPU(StoreRequest $request): array
     {
-        $userBal = DB::table('users')->select('cr_balance')->where('id', '=', $request->user()->id)->get();
-        $userCPU = DB::table('users')->select('cr_cpu')->where('id', '=', $request->user()->id)->get();
+        $userBalRaw = DB::table('users')->select('cr_balance')->where('id', '=', $request->user()->id)->get();
+        $userCPURaw = DB::table('users')->select('cr_cpu')->where('id', '=', $request->user()->id)->get();
+        $userBal = $userBalRaw[0]->cr_balance;
+        $userCPU = $userCPURaw[0]->cr_cpu;
         $cost = $this->credits->get('store:cpu_cost', 20);
 
         if ($userBal < $cost) {
-            return throw new DisplayException('You do not have the required amount of credits.');
+            return [
+                'success' => false,
+                'data' => []
+            ];
         }
-        
+
         DB::table('users')->where('id', '=', $request->user()->id)->update(['cr_cpu' => $userCPU + 50]);
         DB::table('users')->where('id', '=', $request->user()->id)->update(['cr_balance' => $userBal - $cost]);
 
