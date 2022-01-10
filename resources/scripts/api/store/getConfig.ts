0eee66edbdcc3f57ee4eed282ab4aee40bc26354
Jex <cmrxn2005@gmail.com>
2022-01-10 16:48:09 +0000

    Fix API routing for user store configuration

--- resources/scripts/api/store/getConfig.ts
+++ resources/scripts/api/store/getConfig.ts
@@ -1,7 +1,7 @@
 import http from '@/api/http';
 import { ConfigResponse } from '@/components/store/servers/CreateServerContainer';
 
-export default async (id: string): Promise<ConfigResponse> => {
-    const { data } = await http.get(`/api/client/store/config/${id}`);
+export default async (): Promise<ConfigResponse> => {
+    const { data } = await http.get('/api/client/store/config');
     return (data.data || []);
 };
