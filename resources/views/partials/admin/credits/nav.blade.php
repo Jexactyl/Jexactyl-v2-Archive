0f47003e9e411f962fe401f02f68b7b003938b60
JexUK <cmrxn2005@gmail.com>
2022-01-13 00:33:42 +0000

    Fix routes for user navigation bar

--- resources/views/partials/admin/credits/nav.blade.php
+++ resources/views/partials/admin/credits/nav.blade.php
@@ -3,8 +3,8 @@
         <div class="col-xs-12">
             <div class="nav-tabs-custom nav-tabs-floating">
                 <ul class="nav nav-tabs">
-                    <li @if($activeTab === 'config')class="active"@endif><a href="{{ route('admin.credits') }}">Config</a></li>
-                    <li @if($activeTab === 'store')class="active"@endif><a href="{{ route('admin.credits.store') }}">Store</a></li>
+                    <li @if($activeTab === 'config')class="active"@endif><a href="{{ route('admin.users.view') }}">Config</a></li>
+                    <li @if($activeTab === 'store')class="active"@endif><a href="{{ route('admin.users.store') }}">Store</a></li>
                 </ul>
             </div>
         </div>
