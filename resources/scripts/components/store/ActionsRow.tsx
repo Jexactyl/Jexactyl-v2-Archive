428d195f77fdb0b01166be6d30103e7eda5a8ec3
JexUK <cmrxn2005@gmail.com>
2022-01-16 10:29:12 +0000

    Fix CSS on store for mobile devices

--- resources/scripts/components/store/ActionsRow.tsx
+++ resources/scripts/components/store/ActionsRow.tsx
@@ -7,6 +7,7 @@ import useFlash from '@/plugins/useFlash';
 import buySlots from '@/api/store/buy/buySlots';
 import buyRAM from '@/api/store/buy/buyRAM';
 import buyStorage from '@/api/store/buy/buyStorage';
+import tw from 'twin.macro';
 
 const ActionsRow = () => {
     const { addFlash, clearFlashes, clearAndAddHttpError } = useFlash();
@@ -78,44 +79,44 @@ const ActionsRow = () => {
     };
 
     return (
-        <>
-            <div className={'storePurchaseBox'}>
-                <div className={'storePurchaseCol'}>
-                    <TitledGreyBox title={'Purchase Server Slots'} icon={faLayerGroup}>
-                        <Button onClick={() => submitSlots()} disabled={isSubmit}>
-                                1 Slot
-                        </Button>
-                    </TitledGreyBox>
-                </div>
-            </div>
-            <div className={'storePurchaseBox'}>
-                <div className={'storePurchaseCol'}>
-                    <TitledGreyBox title={'Purchase CPU'} icon={faMicrochip}>
-                        <Button onClick={() => submitCPU()} disabled={isSubmit}>
-                                50% CPU
-                        </Button>
-                    </TitledGreyBox>
-                </div>
-            </div>
-            <div className={'storePurchaseBox'}>
-                <div className={'storePurchaseCol'}>
-                    <TitledGreyBox title={'Purchase RAM'} icon={faMemory}>
-                        <Button onClick={() => submitRAM()} disabled={isSubmit}>
-                                1GB RAM
-                        </Button>
-                    </TitledGreyBox>
-                </div>
-            </div>
-            <div className={'storePurchaseBox'}>
-                <div className={'storePurchaseCol'}>
-                    <TitledGreyBox title={'Purchase Storage'} icon={faHdd}>
-                        <Button onClick={() => submitStorage()} disabled={isSubmit}>
-                                1GB Storage
-                        </Button>
-                    </TitledGreyBox>
-                </div>
-            </div>
-        </>
+        <div css={tw`md:flex`}>
+            <TitledGreyBox
+                title={'Purchase Server Slots'}
+                icon={faLayerGroup}
+                css={tw`flex-1`}
+            >
+                <Button onClick={() => submitSlots()} disabled={isSubmit}>
+                    1 Slot
+                </Button>
+            </TitledGreyBox>
+            <TitledGreyBox
+                title={'Purchase CPU'}
+                icon={faMicrochip}
+                css={tw`flex-1 lg:flex-none lg:w-1/4 mt-4 md:mt-0 md:ml-5`}
+            >
+                <Button onClick={() => submitCPU()} disabled={isSubmit}>
+                    50% CPU
+                </Button>
+            </TitledGreyBox>
+            <TitledGreyBox
+                title={'Purchase RAM'}
+                icon={faMemory}
+                css={tw`flex-1 lg:flex-none lg:w-1/4 mt-4 md:mt-0 md:ml-5`}
+            >
+                <Button onClick={() => submitRAM()} disabled={isSubmit}>
+                    1GB RAM
+                </Button>
+            </TitledGreyBox>
+            <TitledGreyBox
+                title={'Purchase Storage'}
+                icon={faHdd}
+                css={tw`flex-1 lg:flex-none lg:w-1/4 mt-4 md:mt-0 md:ml-5`}
+            >
+                <Button onClick={() => submitStorage()} disabled={isSubmit}>
+                    1GB Storage
+                </Button>
+            </TitledGreyBox>
+        </div>
     );
 };
 
