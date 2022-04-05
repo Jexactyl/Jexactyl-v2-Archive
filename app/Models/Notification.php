e18ec845f7de8152a916bee159d0540a3821cd0e
cmrxnn <cmrxn2005@gmail.com>
2022-04-06 00:09:43 +0100

    Create migration and model for user-side notifications

---
+++ app/Models/Notification.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Pterodactyl\Models;
+
+use Ramsey\Uuid\Uuid;
+use Illuminate\Http\Request;
+use Illuminate\Container\Container;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @property int $id
+ * @property int|null $user_id
+ * @property int $server_id
+ * @property string $action
+ * @property array $device
+ * @property array $metadata
+ * @property \Pterodactyl\Models\User|null $user
+ * @property \Pterodactyl\Models\Server|null $server
+ */
+
+class Notification extends Model
+{
+    public const RESOURCE_NAME = 'notifs';
+
+    /**
+     * @var string[]
+     */
+    public static $validationRules = [
+        'user_id' => 'required|integer',
+        'server_id' => 'required|integer',
+        'action' => 'required|string|max:191',
+        'device' => 'array',
+        'device.ip_address' => 'ip',
+        'device.user_agent' => 'string',
+        'metadata' => 'array',
+    ];
+
+    /**
+     * @var string
+     */
+    protected $table = 'notifs';
+
+    /**
+     * @var string[]
+     */
+    protected $casts = [
+        'device' => 'array',
+        'metadata' => 'array',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $guarded = [
+        'id',
+        'created_at',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Creates a new Notification model and returns it, attaching device information and the
+     * currently authenticated user if available. This model is not saved at this point, so
+     * you can always make modifications to it as needed before saving.
+     *
+     * @return $this
+     */
+    public static function instance(string $action, array $metadata, bool $isSystem = false)
+    {
+        /** @var Request $request */
+        $request = Container::getInstance()->make('request');
+
+        if (!$request instanceof Request) {
+            $request = null;
+        }
+
+        return (new self())->fill([
+            'user_id' => ($request && $request->user()) ? $request->user()->id : null,
+            'server_id' => null,
+            'action' => $action,
+            'device' => $request ? [
+                'ip_address' => $request->getClientIp() ?? '127.0.0.1',
+                'user_agent' => $request->userAgent() ?? '',
+            ] : [],
+            'metadata' => $metadata,
+        ]);
+    }
+}
